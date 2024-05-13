80fe70b78bc77db531f4241a2f14367bc195c4ea
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2024-05-13 22:48:23 +0700

    Complete the signin function

--- server/controllers/User.controller.js
+++ server/controllers/User.controller.js
@@ -2,10 +2,12 @@
 const User = require('../models/User.model')
 const bcrypt = require('bcrypt');
 const { errorHandler } = require('../utils/error');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv')
 
+dotenv.config();
 const userControllers = {
   signUp: async (req, res, next) => {
-
     const { username, password, email } = req.body;
     if (!username || !password || !email ||
       email === '' || username === '' || password === '') {
@@ -34,6 +36,43 @@ const userControllers = {
       })
     } catch (e) {
       next(e);
+    }
+  },
+
+  signIn: async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password ||
+      email === '' || password === '') {
+      next(errorHandler(400, 'Tất cả các trường là bắt buộc'));
+    }
+    try {
+      // kiểm tra email đã tồn tại hay chua
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+        return res.status(400).json({
+          message: 'Không tìm thấy người dùng.'
+        })
+      }
+      const validPassword = bcrypt.compareSync(password, validUser.password);
+      if (!validPassword) {
+        return next(errorHandler(400, 'Mật khẩu không hợp lệ.'));
+      }
+      const token = jwt.sign(
+        { userId: validUser._id, isAdmin: validUser.isAdmin },
+        process.env.SECRET_KEY
+      )
+      // không trả ra password
+      const { password: pass, ...rest } = validUser._doc;
+      return res.status(200)
+        .cookie('access_token', token,
+          {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+          })
+        .json(rest);
+    } catch (error) {
+      next(error);
     }
   }
 }
--- server/routes/User.route.js
+++ server/routes/User.route.js
@@ -5,5 +5,6 @@ const router = require('express').Router()
 const userControllers = require('../controllers/User.controller')
 
 router.post('/signup', userControllers.signUp)
+router.post('/signin', userControllers.signIn)
 
 module.exports = router
